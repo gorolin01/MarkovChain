0ce9c4c8fb1f5d149809cff7b97287e60cc0be0e
gorolin01 <gorolin01@gmail.com>
2022-10-04 00:39:01 +0300

    2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,20 @@
 package sample;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static ArrayList<Link> MarkovChain = new ArrayList<>();
 
     public static void readFile(){
+
+       /* try {
+            List<String> a = Files.readAllLines(Paths.get(""));
+        } catch (IOException e){}*/
 
         try (BufferedReader fileReader = new BufferedReader(new FileReader("data.txt"))){
 
@@ -29,21 +36,46 @@ public class Main {
                 }
                 link.setNoda(noda_arr);
                 MarkovChain.add(link);
+                printLink(link);
+                printMarkovChain(MarkovChain);
             }
 
             //test
-            for(int i = 0; i < MarkovChain.size(); i++){
-                System.out.print(MarkovChain.get(i).getWord() + " : ");
-                for(int j = 0; j < MarkovChain.get(i).getNoda().size(); j++){
-                    System.out.print(MarkovChain.get(i).getNoda().get(j).getWord() + ", ");
-                    System.out.print(MarkovChain.get(i).getNoda().get(j).getIterator() + " | ");
-                }
-                System.out.println();
-            }
+            printMarkovChain(MarkovChain);
 
         } catch (IOException e) {
             System.out.println(e);
         }
+
+    }
+
+    public static void printLink(Link link){
+
+        System.out.println("-------------------Link-------------------");
+        System.out.print(link.getWord() + " : ");
+        for(int i = 0; i < link.getNoda().size(); i++){
+            System.out.print(link.getNoda().get(i).getWord() + ", ");
+            System.out.print(link.getNoda().get(i).getIterator() + " | ");
+        }
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        System.out.println();
+
+    }
+
+    public static void printMarkovChain(ArrayList<Link> MarkovChain){
+
+        System.out.println("----------------MarkovChain----------------");
+        for(int i = 0; i < MarkovChain.size(); i++){
+            System.out.print(MarkovChain.get(i).getWord() + " : ");
+            for(int j = 0; j < MarkovChain.get(i).getNoda().size(); j++){
+                System.out.print(MarkovChain.get(i).getNoda().get(j).getWord() + ", ");
+                System.out.print(MarkovChain.get(i).getNoda().get(j).getIterator() + " | ");
+            }
+            System.out.println();
+        }
+        System.out.println("----------------------------------------------");
+        System.out.println();
 
     }
 
